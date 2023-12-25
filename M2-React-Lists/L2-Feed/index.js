e1e5b73263c610727060c40f399f51af047746bc
pavitpim40_mbp <pavitpim@gmail.com>
2023-12-25 15:24:34 +0700

    demo

--- M2-React-Lists/L2-Feed/index.js
+++ M2-React-Lists/L2-Feed/index.js
@@ -19,3 +19,52 @@
 
 const domRoot = document.getElementById('root');
 const root = ReactDOM.createRoot(domRoot);
+
+// Reuse Component => Function Component
+// More Reusable => PassProps
+function Card(props) {
+  return (
+    <div className='card'>
+      {props.header}
+      <div className='card__footer'>
+        <p>author : {props.author}</p>
+        <p>date : {props.date}</p>
+      </div>
+    </div>
+  );
+}
+
+const cardList = [
+  <Card header='A' author='a' date='2023-10-11' />,
+  <Card header='B' author='b' date='2023-12-11' />,
+  <Card header='C' author='c' date='2023-09-11' />,
+];
+
+const feeds = [
+  { id: 1, title: 'A', author: 'a', date: '2023-10-11' },
+  { id: 2, title: 'B', author: 'b', date: '2023-12-11' },
+  { id: 3, title: 'C', author: 'c', date: '2023-09-11' },
+];
+
+root.render(
+  <>
+    {feeds.map(function (card,index) {
+      return <Card 
+					key={index}
+					header={card.title} 
+					author={card.author}
+					date={card.date}
+					/>;
+    })}
+  </>
+);
+
+// <Component a="1" b="2" c="3" d=[]/>
+// Component({ a:"1", b:"2", c:"3", d=[] })
+
+// function Component(ObjectOfAttribute) {
+//   console.log(ObjectOfAttribute);
+//   return <h1>Hi</h1>;
+// }
+
+// root.render(<Component a='1' b='2' c='3' />);
--- M2-React-Lists/L3-Post/index.js
+++ M2-React-Lists/L3-Post/index.js
@@ -22,3 +22,35 @@
 
 const domRoot = document.getElementById('root');
 const root = ReactDOM.createRoot(domRoot);
+
+// // Data
+// const posts = [
+// 	"Hi Thailand", "Hi USA"
+// ];
+
+// item = 	"Hi Thailand"
+// root.render(
+//   <>
+//     {posts.map((item)=> <h1>{item}</h1>)}
+//   </>
+// );
+
+// #############EX2
+// // Data
+const posts = [
+  { id: 1, message: 'Hi Everyone', date: '2023-10-09', author: 'IO' },
+  { id: 2, message: 'Hi Thailand', date: '2023-10-10', author: 'Callan' },
+];
+
+// item = { id: 1, message: 'Hi Everyone', date: '2023-10-09', author: 'IO' }
+root.render(
+  <>
+    {posts.map((item) => (
+      <div className='card'>
+        <h1>{item.message}</h1>
+        <h1>{item.date}</h1>
+        <h1>{item.author}</h1>
+      </div>
+    ))}
+  </>
+);
