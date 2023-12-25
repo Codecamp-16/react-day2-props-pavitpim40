1848655d79eb27990dba8b7480e8a8554ac88aa0
pavitpim40_mbp <pavitpim@gmail.com>
2023-12-25 14:20:28 +0700

    update:sol

--- M2-React-Lists/00_demo/index.js
+++ M2-React-Lists/00_demo/index.js
@@ -195,19 +195,13 @@ const peopleLists = [
     age: 25,
     src: 'https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
   },
-  {
-    id: 4,
-    name: 'John',
-    age: 25,
-    src: 'https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80',
-  },
 ];
 
 function Avatar(props) {
   return (
     <>
       <div className='avatar'>
-        <div className='c'>
+        <div className='image--container'>
           <img src={props.src} />
         </div>
       </div>
